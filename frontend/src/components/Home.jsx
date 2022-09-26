b654e0ac6f006ff43975851ac1f1976a56e27e76
parag <paragpatil187@gmail.com>
2022-09-26 17:49:46 +0530

    navbar with css

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Header from './Header'
+import Navbar from './Header/Navbar'
 
 const Home = () => {
   return (
     <>
-    
+    <Navbar/>
     <Header/>
     </>
   )
