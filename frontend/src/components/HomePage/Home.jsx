5cadc99195949c858888b8ed889709834adec2d4
parag <paragpatil187@gmail.com>
2022-09-28 15:37:39 +0530

    homepage design with static backend data

--- frontend/src/components/HomePage/Home.jsx
+++ frontend/src/components/HomePage/Home.jsx
@@ -1,16 +1,75 @@
-import React from 'react'
-import Header from './Header'
-import Navbar from '../Header/Navbar'
-import Banner from './Banner'
+import { Box, makeStyles } from '@material-ui/core';
+import Header from './Header';
+import Banner from './Banner';
+
+import MidSlide from './MidSlide';
+import MidSection from './MidSection';
+import Slide from './Slide';
+import React,  { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux'; // hooks
+import storeData,{addCart} from "../../redux/data/action"
+import NavBar from './Header';
+
+
+const useStyle = makeStyles({
+    component: {
+        padding: 10,
+        background: '#F2F2F2'
+    }
+})
 
 const Home = () => {
-  return (
-    <>
-    <Navbar/>
-    <Header/>
-    <Banner/>
-    </>
-  )
+  const dispatch = useDispatch();
+    const classes = useStyle();
+    useEffect(()=>{
+      fetch("https://serverfakeappparag.herokuapp.com/products")
+      .then((res)=>res.json())
+      .then((res)=>dispatch(storeData(res)))
+      .catch((err)=>console.log(err))
+     
+ },[])
+
+ const data=useSelector((state)=>state.data.data)
+    
+console.log(data)
+    
+
+    
+
+    return (
+        <> 
+            <NavBar />
+            <Box className={classes.component}>
+                <Banner />
+                <MidSlide products={data} />
+                <MidSection />
+                <Slide
+                    data={data} 
+                    title='Discounts for You'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={data} 
+                    title='Suggested Items'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={data} 
+                    title='Top Selection'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={data} 
+                    title='Recommended Items'
+                    timer={false} 
+                    multi={true} 
+                />
+            </Box>
+        </>
+    )
 }
 
-export default Home
+export default Home;
