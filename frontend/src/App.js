807bab0f695625a331ba05b9915e7e88f1a1724a
parag <paragpatil187@gmail.com>
2022-09-29 14:51:19 +0530

    make some routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Cart from './components/Cart/Cart';
 import Home from './components/HomePage/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/cart" element={<Cart/>}></Route>
+
+
+      </Routes>
+
+      
     </div>
   );
 }
