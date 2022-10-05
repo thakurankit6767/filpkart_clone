3a5926faeb57e7bf1da75c78bf2f9cdd5d2b1a2b
thakurankit6767 <thakurankt6767@gmail.com>
2022-10-05 15:09:54 +0530

    Update App.js
    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Cart from './components/Cart/Cart';
+import Navbar from './components/Header/Navbar';
 import Home from './components/HomePage/Home';
 import ProductView from './components/ItemDetails/ProductView';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
       <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/cart" element={<Cart/>}></Route>
