c036b8a4317b4a5bf5933665dcf33240b8a72c0e
thakurankit6767 <thakurankt6767@gmail.com>
2022-10-17 20:07:14 +0530

    update detailview page
    detailview page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Cart from "./components/Cart/Cart";
-import Footer from "./components/footer/Footer";
+import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Header/Navbar";
 import Home from "./components/HomePage/Home";
 import ProductDetail from "./components/ItemDetails/ProductDetail";
